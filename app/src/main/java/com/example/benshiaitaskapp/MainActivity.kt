849bd5bb039003e7dce6d9c7fabc7f674e102c7a
imevbore <ketuimevbore@yahoo.com>
2022-06-28 22:28:44 +0100

    Initial commit

---
+++ app/src/main/java/com/example/benshiaitaskapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.benshiaitaskapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.findNavController
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navController = findNavController( R.id.nav_host_fragment)
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+
+        return navController.navigateUp()|| super.onSupportNavigateUp()
+    }
+}
